73a5ea347b5f1bf06ee1fc9aa95403f2b38fbb38
stephaniepboyne <stephanie_boyne@yahoo.co.uk>
2022-04-06 16:54:46 +0100

    before css

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Song = ({song}) => {
-
+    
     return <li>
         <p>{song.title.label}</p>
+        <img src={song['im:image'][0].label}/>
+        <button><a href={song.link[1].attributes.href} target='blank'>Download sample</a></button>
+        
     </li>
 }
 
